13ace680510974618210329053c423b72c082c68
vikas0809 <vikasjoshi93@live.com>
2022-11-13 15:29:01 -0500

    Webisite up and running:

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,32 @@
 // import the data from data.js
 const tableData = data;
 // Reference the HTML table using d3
+console.log(tableData);
 var tbody = d3.select("tbody");
-
 function buildTable(data) {
     // First, clear out any existing data
-    tbody.html("");
+    console.log("In the buildtable function");
+    tbody.html(" ");
     // Next, loop through each object in the data
     // and append a row and cells for each value in the row
     data.forEach((dataRow)=>{
         // Append a row to the table body
         let row = tbody.append("tr");
+        console.log("appending rows");
         // Loop through each field in the dataRow and add
         // each value as a table cell (td)
         Object.values(dataRow).forEach((val) => {
             let cell = row.append("td");
+            console.log("appending rows data");
             cell.text(val);
+            console.log(val);
         });
         
     })
 
 };
-
 function handleClick() {
+    console.log("buttonclick");
     let date = d3.select("#datetime").property("value");
     let filteredData = tableData;
      // Check to see if a date was entered and filter the
---
+++ static/js/data.js
@@ -0,0 +1,20 @@
+var data = [
+    {
+      datetime: "1/1/2010",
+      city: "benton",
+      state: "ar",
+      country: "us",
+      shape: "circle",
+      durationMinutes: "5 mins.",
+      comments: "4 bright green circles high in the sky going in circles then one bright green light at my front door."
+    },
+    {
+        datetime: "1/1/2010",
+        city: "bonita",
+        state: "ca",
+        country: "us",
+        shape: "light",
+        durationMinutes: "13 mins.",
+        comments: "Three nright red lights witnessed floating stationary over San Diego New Years Day 2010."
+      },
+]
